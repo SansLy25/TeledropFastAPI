983af708e398d80f86b41a2722246ea4ca40e58e
Sansly25 <robertstepa017@gmail.com>
2025-07-23 04:40:34 +0300

    fix(ListFileView): fix edit icon

--- frontend/src/components/files/FileSystemItem.jsx
+++ frontend/src/components/files/FileSystemItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { EllipsisVertical } from "lucide-react";
 
 function FileSystemItem({ name, icon, alt, view = "big", editable = true }) {
     if (view === "big") {
         return (
             <div className="flex flex-col items-center justify-start p-3 cursor-pointer hover:bg-gray-100 dark:hover:bg-neutral-900 rounded-lg transition-colors">
-                <img src={icon} alt={alt} className="w-21 h-21" />
+                <div className="relative">
+                    <img src={icon} alt={alt} className="w-21 h-24" />
+                    <button onClick={() => alert("клик")} className="absolute right-[-1.0rem] top-3/5 -translate-y-1/2">
+                        <EllipsisVertical width="20px" height="20px"/>
+                    </button>
+                </div>
                 <div className="mt-1 text-center line-clamp-2 text-[0.65rem] font-medium max-w-[120px] sm:max-w-[150px]">
                     {name}
                 </div>
--- frontend/src/components/files/ListFileView.jsx
+++ frontend/src/components/files/ListFileView.jsx
@@ -5,7 +5,7 @@ import Folder from "./Folder";
 function ListFileView({files = [], folders = [], edit = true, view = "big"}) {
     return (
         <div className={view === "big" 
-            ? "w-full grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-4 mt-2.5"
+            ? "w-full ml-[-0.15rem] grid grid-cols-3 overflow-auto sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-4 mt-2.5"
             : "w-full flex flex-col gap-2 mt-2"
         }>
             {folders.map((folder, index) => (
