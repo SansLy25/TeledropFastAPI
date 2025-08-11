f147ed43e17a121aabef5138e809e109b1360b79
Sansly25 <robertstepa017@gmail.com>
2025-08-11 17:21:24 +0300

    feat(ContextMenu): add logic

--- frontend/src/components/files/ListFileView.jsx
+++ frontend/src/components/files/ListFileView.jsx
@@ -5,8 +5,8 @@ import Folder from "./Folder";
 function ListFileView({files = [], folders = [], edit = true, view = "big"}) {
     return (
         <div className={view === "big" 
-            ? "w-full ml-[-0.15rem] grid grid-cols-3 overflow-auto sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-4 mt-2.5"
-            : "w-full flex flex-col gap-2 mt-2"
+            ? "w-full ml-[-0.15rem] overflow-x-hidden grid grid-cols-3 h-[calc(100vh-17rem)] no-scrollbar overflow-y-auto sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-4 mt-2.5"
+            : "w-full flex flex-col gap-2 mt-2 h-[calc(100vh-12rem)] overflow-y-auto"
         }>
             {folders.map((folder, index) => (
                 <Folder 
--- frontend/src/components/ui/ContextMenu.jsx
+++ frontend/src/components/ui/ContextMenu.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function ContextMenu({children, actionElement}) {
+    const [isOpened, setIsOpened] = useState(false);
+    const [actionElementSize, setActionElementSize] = useState({ width: 0, height: 0 });
+    const actionElementRef = useRef(null);
+
+    function onActionElementClick(event) {
+        setIsOpened(!isOpened);
+    }
+
+    const enhancedActionElement = React.cloneElement(actionElement, {
+        onClick: onActionElementClick,
+        ref: actionElementRef,
+    });
+
+    useEffect(() => {
+        if (actionElementRef.current) {
+            const { offsetWidth, offsetHeight } = actionElementRef.current;
+            setActionElementSize({ width: offsetWidth, height: offsetHeight });
+        }
+    }, [isOpened]);
+
     return (
-        actionElement
-    )
+        <div className="relative">
+            {enhancedActionElement}
+            {isOpened && (
+                <div
+                    className="absolute size-10 rounded-xl bg-amber-100"
+                    style={{ top: actionElementSize.height + 'px', left: "0",  zIndex: "9999"}}
+                >
+                    {children}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default ContextMenu;
